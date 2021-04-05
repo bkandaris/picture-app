764a4d4c07702ea42c112311c8a920ff909576a2
Benjamin Kandaris <benjaminkandaris@yahoo.com>
2021-04-05 14:13:57 -0400

    styled chat component

--- src/Chat.js
+++ src/Chat.js
@@ -1,15 +1,19 @@
 import React from 'react';
 import "./Chat.css";
 import Avatar from "@material-ui/core/Avatar";
+import StopRoundedIcon from "@material-ui/icons/StopRounded"
+import ReactTimeago from 'react-timeago';
 
 function Chat({id, username, timestamp, read, imageUrl, profilePic}){
     return (
-        <div>
-            <Avatar src={profilePic} />
+        <div onClick={()=>{console.log("pholder")}} className="chat">
+            <Avatar className='chat__avatar' src={profilePic} />
             <div className="Chat__info">
                 <h4>{username}</h4>
-                <p>Tap to View - {new Date(timestamp?.toDate()).toUTCString()}</p>
+                <p>Tap to View - <ReactTimeago date={new Date(timestamp?.toDate()).toUTCString()} /></p>
             </div>
+
+            {!read && <StopRoundedIcon className="chat__readIcon" />}
         </div>
     )
 }
